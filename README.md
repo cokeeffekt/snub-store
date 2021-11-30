a32c2aa29dcd5fbfb28614225ba7cb1fe4f4a877
Patrik Hartwig <git@pat.wtf>
2021-11-30 15:57:43 +1000

    init

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # snub-store
+
 Snub simple synced key val storage
+
+## Basics
+
+Allow you to store strings, numbers, arrays and non complex object to a persistent store live and accessible on all instances.
+Before anyone gets any brilliant ideas to use this for caching, know that all the values are kept in memory to maintain syncrhonous access.
+
+## Set a key store
+
+```javascript
+snub.store.someString = 'Thanks for all the fish';
+snub.store.someNumber = 42;
+snub.store.someObj = { hello: 'world' };
+snub.store.someArray = [1, 2, 3, 4];
+```
+
+## Get a key store
+
+```javascript
+console.log(snub.store.someString);
+// 'Thanks for all the fish'
+```
+
+## Delete a key store
+
+```javascript
+delete snub.store.someString;
+```
+
+## extra notes
+
+you cant mutate nested structures on stores.
---
+++ snub-store.js
@@ -0,0 +1,57 @@
+module.exports = function (config) {
+  config = Object.assign(
+    {
+      namespace: '',
+    },
+    config || {}
+  );
+
+  var tracked = {};
+  return function (snub) {
+    var localStore = {};
+
+    sync(true);
+
+    snub.store = new Proxy(localStore, {
+      get: function (target, prop) {
+        return JSON.parse(target[prop]);
+      },
+      set: async function (target, prop, value) {
+        value = JSON.stringify(value);
+        if (target[prop] === value) return;
+        target[prop] = value;
+        snub.poly('store_internal:SET', [prop, value]).send();
+        await snub.redis.hset('_snub-store::' + config.namespace, prop, value);
+      },
+      deleteProperty: function (target, prop) {
+        snub.redis.hdel('_snub-store::' + config.namespace, prop);
+        snub.poly('store_internal:DEL', prop).send();
+        delete target[prop];
+      },
+    });
+    snub.storeSync = function () {
+      sync();
+    };
+
+    snub.on('store_internal:DEL', function (prop) {
+      delete localStore[prop];
+    });
+    snub.on('store_internal:SET', function (a) {
+      var [prop, value] = a;
+      localStore[prop] = value;
+    });
+
+    async function sync(init) {
+      var persisted = await snub.redis.hgetall(
+        '_snub-store::' + config.namespace
+      );
+      if (!init)
+        for (var lkey of Object.keys(localStore)) {
+          if (!persisted.has(lkey)) delete localStore[lkey];
+        }
+      for (var pkey of Object.keys(persisted)) {
+        localStore[pkey] = persisted[pkey];
+      }
+    }
+  };
+};
